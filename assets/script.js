e96b703f56f9ef268d42d9e17c58f3b0b0ca7cbc
Mitchell Wood <mitchellwood11@gmail.com>
2022-10-10 18:40:57 -0500

    question-cycling-error

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-var quizIntro = document.getElementsByClassName("quiz-intro")[0]
-var quizContent = document.getElementsByClassName("quiz-content")[0]
+var quizIntro = document.getElementsByClassName("quiz-intro")[0];
+var quizContent = document.getElementsByClassName("quiz-content")[0];
 var startButton = document.getElementById("startButton");
-var question = document.getElementById("question")
-var answerOne = document.getElementById("answer-1")
-var answerTwo = document.getElementById("answer-2")
-var answerThree = document.getElementById("answer-3")
-var answerFour = document.getElementById("answer-4")
-var currentQuestion = 0
+var question = document.getElementById("question");
+var answerOne = document.getElementById("answer-1");
+var answerTwo = document.getElementById("answer-2");
+var answerThree = document.getElementById("answer-3");
+var answerFour = document.getElementById("answer-4");
+var currentQuestion = 0;
 startButton.addEventListener("click", mainQuiz);
 
 var questions = [{
@@ -17,29 +17,40 @@ var questions = [{
 ,{
     question: "what color is the ground?",
     answers: ["blue", "red", "purple", "green"],
-    correctAnswer: 3 
+    correctAnswer: 3
+}
+,{
+    question: "what color is the ocean?",
+    answers: ["red", "blue", "purple", "green"],
+    correctAnswer: 1
+}
+,{
+    question: "what color is my truck?",
+    answers: ["red", "silver", "orange", "blue"],
+    correctAnswer: 1
+}
+,{
+    question: "what color is the street?",
+    answers: ["blue", "orange", "black", "yellow"],
+    correctAnswer: 2
 }]
 
 function mainQuiz () {
     quizIntro.style.display = "none"
     quizContent.style.display = "block"
     goToQuestion()
-
-
-    // $("button").click(function() {
-    //     var button = $(this).val();
-    //     alert(button);
-    // });
 }
 
-function chooseAnswer (answer) {
+function chooseAnswer () {
    currentQuestion++
    goToQuestion()
-   console.log(answer.target.value)
-   console.log(currentQuestion)
 }
 
 function goToQuestion() {
+    if (currentQuestion > 5) {
+        endQuiz()
+        return
+        }
     question.innerHTML = questions[currentQuestion].question
     answerOne.innerHTML = questions[currentQuestion].answers[0]
     answerTwo.innerHTML = questions[currentQuestion].answers[1]
@@ -49,4 +60,7 @@ function goToQuestion() {
     answerTwo.addEventListener("click", chooseAnswer);
     answerThree.addEventListener("click", chooseAnswer);
     answerFour.addEventListener("click", chooseAnswer);
+}
+function endQuiz() {
+    console.log("is is working")
 }
