7acca54a24581b242673ec834090f2dbae5a9d53
Mitchell Wood <mitchellwood11@gmail.com>
2022-10-10 04:32:22 -0500

    initial-js-3

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
+var quizIntro = document.getElementsByClassName("quiz-intro")[0]
+var quizContent = document.getElementsByClassName("quiz-content")[0]
 var startButton = document.getElementById("startButton");
 startButton.addEventListener("click", runQuiz);
 
 function runQuiz () {
-    var quizIntro = document.getElementsByClassName("quiz-intro")[0]
-    var quizContent = document.getElementsByClassName("quiz-content")[0]
     quizIntro.style.display = "none"
     quizContent.style.display = "block"
 }
